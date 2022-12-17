f4567285ed57628de16e9f8de1312b8e4c08f1d7
Mateus Rodrigues <mateus.fr26@gmail.com>
2022-12-17 15:53:31 -0300

    Move EagerLoadQuery to its own file

--- Sources/Nuvem/CKQueryBuilder.swift
+++ Sources/Nuvem/CKQueryBuilder.swift
@@ -1,36 +1,5 @@
 import CloudKit
 
-struct EagerLoadQuery<Model> {
-    
-    let fieldKeyPath: PartialKeyPath<Model>
-    
-    let desiredKeys: [CKRecord.FieldKey]?
-        
-    init<Value>(field: KeyPath<Model, CKReferenceField<Value>>) {
-        self.fieldKeyPath = field
-        self.desiredKeys = nil
-    }
-    
-    init<Value>(field: KeyPath<Model, CKReferenceField<Value>>, desiredFields: PartialKeyPath<Value>...) {
-        self.fieldKeyPath = field
-        self.desiredKeys = desiredFields.map({ CKFieldPath($0).key })
-    }
-        
-    func run(for referenceFields: [any CKReferenceFieldProtocol], on database: CKDatabase) async throws {
-        
-        let idsToFetch = Set(referenceFields.compactMap(\.reference?.recordID))
-                
-        let response = try await database.records(for: Array(idsToFetch), desiredKeys: desiredKeys)
-        
-        for field in referenceFields {
-            if let id = field.reference?.recordID, let record = try response[id]?.get() {
-                field.initialiseValue(record)
-            }
-        }
-        
-    }
-}
-
 public class CKQueryBuilder<Model> where Model: CKModel {
     
     let database: CKDatabase
