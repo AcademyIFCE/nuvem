bfac2d0ec4f792724b6576c5ebc6a9fcb3351c66
Mateus Rodrigues <mateus.fr26@gmail.com>
2023-02-15 11:26:56 -0300

    Remove testExample

--- Tests/NuvemTests/NuvemTests.swift
+++ Tests/NuvemTests/NuvemTests.swift
@@ -3,10 +3,5 @@ import CloudKit
 @testable import Nuvem
 
 final class NuvemTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(Nuvem().text, "Hello, World!")
-    }
+    
 }
